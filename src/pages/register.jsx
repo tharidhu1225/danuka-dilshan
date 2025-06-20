cc37f497fb0dad5876d47f17403c4f3b33ecbf87
tharidhu1225 <jayasooriyatharidhu@gmail.com>
2025-06-20 21:56:14 +0530

    Update Route register page

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,9 +1,3 @@
-import { useGoogleLogin } from '@react-oauth/google';
-import axios from 'axios';
-import { useState } from 'react';
-import toast from 'react-hot-toast';
-import { Link } from 'react-router-dom';
-
 export default function Register() {
  
   return (
@@ -12,50 +6,10 @@ export default function Register() {
         <div className="text-center">
           <img src="/logo.jpg" alt="Logo" className="w-16 h-16 mx-auto rounded-full mb-2" />
           <h1 className="text-2xl font-bold">Welcome to <span className="text-black">Dilsha Blog</span></h1>
-          <p className="text-sm text-gray-500">Login to your account</p>
+          <p className="text-sm text-gray-500">This Page Is Currently Unavailable</p>
+          <span className='text-red-400 font-[800] text-[30px]'>Commin soon</span>
         </div>
-
-        <div className="space-y-4">
-          <div>
-            <label className="block text-sm font-medium">Email</label>
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="w-full mt-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black"
-              placeholder="you@example.com"
-            />
-          </div>
-
-          <div>
-            <label className="block text-sm font-medium">Password</label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full mt-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black"
-              placeholder="••••••••"
-            />
-          </div>
-
-          <button
-            onClick={login}
-            className="w-full bg-black text-white py-2 rounded-md hover:bg-gray-800 transition duration-200"
-          >
-            Login
-          </button>
-
-          <button
-            onClick={() => googleLogin()}
-            className="w-full border border-black py-2 rounded-md hover:bg-black hover:text-white transition duration-200"
-          >
-            Login with Google
-          </button>
-        </div>
-
-        <div className="text-center text-sm text-gray-500">
-          Don't have an account? <Link to="/register" className="text-black underline">Register</Link>
-        </div>
+          
       </div>
     </div>
   );
